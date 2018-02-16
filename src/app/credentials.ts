3cc72273fe17c28fe6255e3cc040a5ef8126890a
gauravku <gaurav.kumar@ieee.org>
2018-02-16 05:21:07 -0500

    credential file

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD9t8osVsHVU56VQTlcf3tloRd-cswl_p8",
-  authDomain: "ionic-auth-61be6.firebaseapp.com",
-  databaseURL: "https://ionic-auth-61be6.firebaseio.com",
-  projectId: "ionic-auth-61be6",
-  storageBucket: "",
-  messagingSenderId: "483143381043"
+  apiKey: "Your Api Key Here",
+  authDomain: "Your Auth Domain",
+  databaseURL: "Your Database Url",
+  projectId: "Your Project Id",
+  storageBucket: "Your Storage Bucket",
+  messagingSenderId: "Your messaging Id"
 };
